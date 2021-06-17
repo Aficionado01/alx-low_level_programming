bfa0d2fec741cfec2f0212f0c9e7a02f6f316466
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-06-17 13:36:38 +0100

    Completed task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string with printf
+ *
+ * Return: 0 if program executed successfully
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
