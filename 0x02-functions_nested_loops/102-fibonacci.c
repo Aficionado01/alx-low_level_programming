358a580a8343f8f7b7b5e63ff702fc142674f92c
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-06-22 12:59:47 +0100

    Completed task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fionacci numbers
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int limit = 50;
+	long long int num1 = 0;
+	long long int num2 = 1;
+	int i;
+
+	for (i = 0; i < limit; i++)
+	{
+		printf("%lld%s", num1 + num2, i == limit - 1 ? "\n" : ", ");
+		int temp = num1;
+		num1 = num2;
+		num2+= temp;
+	}
+
+	return (0);
+}
