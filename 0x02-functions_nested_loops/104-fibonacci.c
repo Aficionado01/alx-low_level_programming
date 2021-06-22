97b954ea38b25e9d72a69526ab605c6fe971d049
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-06-22 17:56:08 +0100

    Fixed task 16's value overflow

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,65 @@
 #include <stdio.h>
 #include <stdint.h>
 
+void print_128(__int128 num);
+__int128 b10_pow_128(int idx);
+
 /**
- * main - prints the first 50 fibonacci numbers
+ * main - prints the first 98 fibonacci numbers
  *
  * Return: 0 if successful
  */
 int main(void)
 {
 	int limit = 98;
-	uint64_t num1 = 0;
-	uint64_t num2 = 1;
+	__int128 num1 = 0;
+	__int128 num2 = 1;
 	int i;
 
 	for (i = 0; i < limit; i++)
 	{
-		uint64_t temp = num1;
+		__int128 temp = num1;
+		__int128 current_num = num1 + num2;
 
-		printf("%lu%s", num1 + num2, i == limit - 1 ? "\n" : ", ");
+		if (i < limit)
+		{
+			print_128(current_num);
+			printf("%s", i == limit - 1 ? "\n" : ", ");
+		}
+
 		num1 = num2;
-		num2+= temp;
+		num2 += temp;
 	}
 
 	return (0);
+}
+
+void print_128(__int128 num)
+{
+	__int128 rem = num;
+	int i;
+
+	for (i = 37; i >= 0; i--)
+	{
+		__int128 power = b10_pow_128(i);
+
+		if (rem > power - 1){
+			putchar((int)((rem / power) % 10) + '0');
+		}
+	}
+}
+
+__int128 b10_pow_128(int idx)
+{
+	int i;
+	__int128 result = 1;
+
+	if (idx < 0)
+		return 0;
+
+	for (i = 0; i < idx; i++)
+	{
+		result *= 10;
+	}
+	return result;
 }
