600e36a82b2060e3229a2b85de1486eecf8e68ba
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-06-28 14:58:35 +0100

    Reduced task 10's code size

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -9,5 +9,20 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int b10_pow(int idx);
 int _atoi(char *s);
+
+/**
+ * b10_pow - Computes the power of 10 to the given index
+ * @idx: The index to which 10 is raised to.
+ *
+ * Return: A power of 10
+ */
+static int b10_pow(int idx)
+{
+	int i;
+	int result = 1;
+
+	for (i = 0; i < idx; i++)
+		result *= 10;
+	return (idx < 0 ? 0 : result);
+}
