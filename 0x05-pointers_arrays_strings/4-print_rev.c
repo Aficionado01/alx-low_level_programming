eb42f4a9bdf6ff9ebb8affd29440bb7403c6f193
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-06-28 08:06:45 +0100

    Completed task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a newline
+ * @s: The string to be printed
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*(s + length) != '\0')
+		length++;
+
+	while (length > 0)
+	{
+		_putchar(*(s + length - 1));
+		length--;
+	}
+
+	_putchar('\n');
+}
