3df259cfd17d1fdf7c443a9479924a468cb4d62a
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-01 16:00:11 +0100

    Updated algorithm

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,3 @@
-#include "holberton.h"
-
 /**
  * infinite_add - Adds two numbers
  * @n1: The first number
@@ -22,8 +20,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	i = max_len;
 	if (size_r < i + 1)
 		return (0);
-	*(r + i) = '\0';
-	i--;n1_len--;n2_len--;
+	*(r + i) = '\0'; i--; n1_len--;n2_len--;
 	while (i >= 0) {
 		n1_dig = n1_len >= 0 ? *(n1 + n1_len) - '0' : 0;
 		n2_dig = n2_len >= 0 ? *(n2 + n2_len) - '0' : 0;
