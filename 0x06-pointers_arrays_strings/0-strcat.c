b160855824f634ba9c380b7016373b3e15dcfef7
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-01 04:49:09 +0100

    Fixed coding style

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,8 +15,11 @@ char *_strcat(char *dest, char *src)
 	while (*(dest + dest_length) != '\0')
 		dest_length++;
 
-	while (*(src + src_length++) != '\0')
+	while (*(src + src_length) != '\0')
+	{
 		*(dest + dest_length + src_length) = *(src + src_length);
+		src_length++;
+	}
 	*(dest + dest_length + src_length) = '\0';
 	return (dest);
 }
