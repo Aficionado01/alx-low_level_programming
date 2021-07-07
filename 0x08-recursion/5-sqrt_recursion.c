886f8261680444887de670346456c1d0fa19b70b
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-07 19:57:27 +0100

    Completed task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,26 +2,14 @@
 
 /**
  * _sqrt_recursion_update - Computes the square root of a number
+ * @i: The index
  * @n: The radicand
  *
  * Return: The square root of the number
  */
-double _sqrt_recursion_update(double n)
+int _sqrt_recursion_update(int i, int n)
 {
-	double tolerance = 0.1;
-	double root = n;
-	double res_diff = n - root * root;
-	double res_diff_abs = res_diff < 0 ? -1 * res_diff : res_diff;
-
-	update:
-	if (res_diff_abs > tolerance)
-	{
-		root = (root + n / root) / 2;
-		res_diff = n - root * root;
-		res_diff_abs = res_diff < 0 ? -1 * res_diff : res_diff;
-		goto update;
-	}
-	return (root);
+	return (i * i >= n ? i : _sqrt_recursion_update(i + 1, n));
 }
 
 /**
@@ -38,10 +26,10 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		double root = _sqrt_recursion_update(n);
+		int root = _sqrt_recursion_update(0, n);
 
-		if ((long)root * (long)root == n)
-			return ((int)root);
+		if (root * root == n)
+			return (root);
 		else
 			return (-1);
 	}
