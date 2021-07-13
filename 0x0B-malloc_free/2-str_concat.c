87b87f1c95c00ff4e302ac5f9ee1229a11cb71e8
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-13 09:25:25 +0100

    Fixed null strings issue

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,17 +14,17 @@ char *str_concat(char *s1, char *s2)
 	int i, len;
 	char *str;
 
-	for (len = 0; *(s1 + len) != '\0'; len++)
+	for (len = 0; *(s1 + len) != '\0' && s1 != NULL; len++)
 		;
-	for (i = 0; *(s2 + i) != '\0'; i++)
+	for (i = 0; *(s2 + i) != '\0' && s2 != NULL; i++)
 		;
 	len += i;
 	str = malloc(sizeof(char) * len + 1);
 	if (str)
 	{
-		for (len = 0; *(s1 + len) != '\0'; len++)
+		for (len = 0; *(s1 + len) != '\0' && s1 != NULL; len++)
 			*(str + len) = *(s1 + len);
-		for (i = 0; *(s2 + i) != '\0'; i++)
+		for (i = 0; *(s2 + i) != '\0' && s2 != NULL; i++)
 			*(str + len + i) = *(s2 + i);
 		len += i;
 		*(str + len) = '\0';
