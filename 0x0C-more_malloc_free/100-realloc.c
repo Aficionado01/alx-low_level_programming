b7d5b84f3e4f4967e9640782055e91bc45e755e1
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-16 00:11:14 +0100

    Updated algorithm

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -36,12 +36,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else
 	{
-		new_ptr = malloc(old_size);
-		if (new_ptr != NULL)
-			return (new_ptr);
 		new_ptr = malloc(new_size);
-		if (new_ptr != NULL)
-			return (new_ptr);
-		return (NULL);
+		return (new_ptr != NULL ? new_ptr : NULL);
 	}
 }
