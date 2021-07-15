e50bf0c4dbee9f8ef70ee2474c98cae1b28fea6f
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-15 23:32:40 +0100

    Fixed coding style

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,9 +20,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len = 0;
 	for (len = 0; *(s1 + len) != '\0'; len++)
 		continue;
-	for (i = 0; *(s2 + i) != '\0' && i < n; i++)
-		len++;
-	str = malloc(sizeof(char) * (len + 1));
+	/* for (i = 0; *(s2 + i) != '\0' && i < n; i++) */
+	/* 	len++; */
+	str = malloc(sizeof(char) * (len + n 1));
 	if (str != NULL)
 	{
 		for (len = 0; *(s1 + len) != '\0'; len++)
