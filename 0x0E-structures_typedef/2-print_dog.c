b16e79f48176f9aadd792bb770fd91eea10fdd9b
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-19 12:14:13 +0100

    :wrench: Fixed compilation issues

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,9 +10,6 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	if ((void *)d->age != NULL)
-		printf("Age: %f\n", d->age);
-	else
-		printf("Age: %s\n", "(nil)");
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->name != NULL ? d->owner : "(nil)");
 }
