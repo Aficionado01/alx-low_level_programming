14c82e95715b928785a77179e2595dc2241a6386
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-19 12:12:48 +0100

    :wrench: Added dog header

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * print_dog - Prints a variable of type struct dog
@@ -8,10 +9,10 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	if (d->age)
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	if ((void *)d->age != NULL)
 		printf("Age: %f\n", d->age);
 	else
 		printf("Age: %s\n", "(nil)");
-	printf("Owner: %s\n", d->name ? d->owner : "(nil)");
+	printf("Owner: %s\n", d->name != NULL ? d->owner : "(nil)");
 }
