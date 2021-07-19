31f16be671b4d037916295c6e28c0c4a477dd048
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-19 20:50:54 +0100

    :wrench: Added support for NULL parameters

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,11 +12,11 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	int name_len, owner_len;
+	int name_len = 0, owner_len = 0;
 
-	for (name_len = 0; *(name + name_len) != '\0'; name_len++)
+	for (name_len = 0; name && *(name + name_len) != '\0'; name_len++)
 		;
-	for (owner_len = 0; *(owner + owner_len) != '\0'; owner_len++)
+	for (owner_len = 0; owner && *(owner + owner_len) != '\0'; owner_len++)
 		;
 	dog = malloc(sizeof(dog_t));
 	if (dog)
@@ -27,7 +27,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(dog);
 			return (NULL);
 		}
-		for (name_len = 0; *(name + name_len) != '\0'; name_len++)
+		for (name_len = 0; name && *(name + name_len) != '\0'; name_len++)
 			*(dog->name + name_len) = *(name + name_len);
 		*(dog->name + name_len) = '\0';
 		dog->age = age;
@@ -38,7 +38,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(dog);
 			return (NULL);
 		}
-		for (owner_len = 0; *(owner + owner_len) != '\0'; owner_len++)
+		for (owner_len = 0; owner && *(owner + owner_len) != '\0'; owner_len++)
 			*(dog->owner + owner_len) = *(owner + owner_len);
 		*(dog->owner + owner_len) = '\0';
 	}
