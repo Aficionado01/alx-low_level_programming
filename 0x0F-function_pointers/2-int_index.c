efd35d673e13bd61c9a949ab93a85b72974af789
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-20 16:41:39 +0100

    :wrench: Fixed algorithm

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (!cmp || size <= 0)
+	if (!cmp || size <= 0 || !array)
 		return (-1);
 	while (cmp(*(array + i)) == 0 && i < size)
 		i++;
