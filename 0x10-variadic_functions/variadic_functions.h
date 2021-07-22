72b65ae6af5433a3c13dd1c2acdffea6a30f9df3
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-22 11:44:56 +0100

    :broom: Refactored algorithm

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,16 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+/**
+ * struct format_printer - A structure for a type formatted printer
+ * @type: The type of data to be formatted
+ * @func: The format printing function
+ */
+struct format_printer
+{
+	char type;
+	void (*func)(va_list);
+};
+typedef struct format_printer fmt_printer_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
