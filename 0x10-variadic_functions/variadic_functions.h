e2142a73b61437083d0aa03995d3de6d52927b82
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-22 12:14:16 +0100

    :broom: Refactored: added functions to header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,5 +14,9 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_char(va_list *args);
+void print_integer(va_list *args);
+void print_float(va_list *args);
+void print_string(va_list *args);
 void print_all(const char * const format, ...);
 #endif
