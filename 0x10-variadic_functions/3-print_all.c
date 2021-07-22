ba4be7782dabec74a6c8312fb08e5ee774fd7063
bezxx_chihuahua <bezaleeloci@gmail.com>
2021-07-22 13:14:43 +0100

    :broom: Refactored code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -37,12 +37,9 @@ void print_string(va_list *args)
 {
 	char *str = va_arg(*args, char *);
 
-	if (str)
-	{
-		printf("%s", str);
-		return;
-	}
-	printf("(nil)");
+	if (!str)
+		str = "(nil)";
+	printf("%s", str);
 }
 
 /**
