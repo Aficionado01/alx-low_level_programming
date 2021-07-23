27b3a16cbbbe5e9997e1adb3c8c87a6358087bc1
Aficionado01 <emmanuelugochukwu530gmail.com>
2021-07-22 20:53:14 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,7 @@
-include "holberton.h"
+#include "holberton.h"
 
 /**
-* print_square - check the code for Holberton School students.
+ * print_square - check the code for Holberton School students.
 * @n : int
 * Return: Always 0.
 */
