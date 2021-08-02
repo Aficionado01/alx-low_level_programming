66698a6e5abded5965eb70a6e670b99d5a82f2e1
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-08-02 06:12:40 +0100

    :broom: Fixed function name

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a listint_t list
+ * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: The node at the beginning of the list
  * @n: The value to be contained in the new node
  *
  * Return: The pointer to the new node otherwise, NULL
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	unsigned int size;
 	listint_t *new_node;
