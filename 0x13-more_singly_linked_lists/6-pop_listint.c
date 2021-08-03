e3f9baca869efb5837f807b41938ac22c29b1c79
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-08-03 07:43:18 +0100

    :wrench: Fixed NULL ref issue

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -16,8 +16,7 @@ int pop_listint(listint_t **head)
 	{
 		node = *head;
 		val = node->n;
-		if ((*head)->next)
-			*head = (*head)->next;
+		*head = (*head)->next;
 		free(node);
 	}
 	return (val);
