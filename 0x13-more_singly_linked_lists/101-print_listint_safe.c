4a52b6b476884071110fce5f8863e8e22bca9523
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-08-03 11:40:04 +0100

    :wrench: Fixed Betty issues

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,67 +4,6 @@
 #include "lists.h"
 
 /**
- * _realloc - Reallocates a memory block
- * @ptr: The pointer to the previous memory block
- * @old_size: The size of the old memory block
- * @new_size: The size of the new memory block
- *
- * Return: The pointer to the new memory block otherwise NULL
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *new_ptr;
-	unsigned int min_size = old_size < new_size ? old_size : new_size;
-	unsigned int i;
-
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr != NULL)
-	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		new_ptr = malloc(new_size);
-		if (new_ptr != NULL)
-		{
-			for (i = 0; i < min_size; i++)
-				*((char *)new_ptr + i) = *((char *)ptr + i);
-			free(ptr);
-			return (new_ptr);
-		}
-		free(ptr);
-		return (NULL);
-	}
-	else
-	{
-		new_ptr = malloc(new_size);
-		return (new_ptr);
-	}
-}
-
-/**
- * exists - Checks if an item exists in the given array
- * @arr: The array to look in
- * @n: The lentgh of the array
- * @item: The item to look for
- *
- * Return: 1 if it exists, otherwise, 0
- */
-char exists(void **arr, size_t n, void *item)
-{
-	size_t i;
-
-	for (i = 0; i < n; i++)
-	{
-		if (*(arr + i) == item)
-			return (1);
-	}
-	return (0);
-}
-
-/**
  * print_ptr - Prints a pointer
  * @ptr: The pointer to print
  * @stage: The stage in the printing task
