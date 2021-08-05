b366b2bf8750bb531651faf764eb4ed11b3191a1
Aficionado01 <emmanuelugochukwu530gmail.com>
2021-08-05 11:14:47 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,31 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-* binary_to_uint - converts a binary nbr to an unsigned int.
-* @b: pointer to a string containing a binary nbr
+* binary_to_uint - Converts a binary number to an unsigned int.
+* @b: A pointer to a string of 0 and 1 chars.
 *
-* Return: unsigned int of binary nbr, or 0
+* Return: If b is NULL or contains chars not 0 or 1 - 0.
+*         Otherwise - the converted number.
 */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int ui = 0;
+unsigned int num = 0, mult = 1;
+int len;
 
 if (!b)
 return (0);
-i = 0;
-while (b[i] != '\0')
-{
-if (b[i] != '0' && b[i] != '1')
-return (0);
-i++;
-}
-for (i = 0; b[i] != '\0'; i++)
-{
-ui <<= 1;
-if (b[i] == '1')
-ui += 1;
 
+for (len = 0; b[len];)
+len++;
+
+for (len -= 1; len >= 0; len--)
+{
+if (b[len] != '0' && b[len] != '1')
+return (0);
+
+num += (b[len] - '0') * mult;
+mult *= 2;
 }
-return (ui);
+
+return (num);
 }
