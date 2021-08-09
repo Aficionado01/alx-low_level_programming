ed319419a446a1e31eecc327fe502648145e4ec6
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-08-09 21:46:28 +0100

    :wrench: Quick patch

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_TRUNC);
-	if (fd < 0)
-		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fd >= 0)
 	{
 		for (i = 0; text_content != NULL && *(text_content + i) != '\0'; i++)
@@ -23,7 +21,9 @@ int create_file(const char *filename, char *text_content)
 			if (write(fd, text_content + i, 1) != 1)
 				return (-1);
 		}
-		close(fd);
+		if (close(fd) == -1)
+			return (-1);
+		return (1);
 	}
-	return (1);
+	return (-1);
 }
