7661d99a45541a567299358ac81f166c864221f8
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-08-09 13:02:50 +0100

    :wrench: Fixed algorithm

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,14 +15,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	fd = open(filename, O_RDWR | O_TRUNC);
 	if (fd < 0)
-	{
-		close(fd);
 		fd = open(filename, O_WRONLY | O_CREAT, 0600);
-		for (i = 0; text_content != NULL && *(text_content + i) != '\0'; i++)
-		{
-			if (write(fd, text_content + i, 1) != 1)
-				return (-1);
-		}
+	for (i = 0; text_content != NULL && *(text_content + i) != '\0'; i++)
+	{
+		if (write(fd, text_content + i, 1) != 1)
+			return (-1);
 	}
 	close(fd);
 	return (1);
