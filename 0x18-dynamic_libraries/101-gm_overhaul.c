66a9a08941f0d90298997e9e2b5d400b11527850
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-20 12:51:11 +0100

    :wrench: Updated code injection files

--- 0x18-dynamic_libraries/101-gm_overhaul.c
+++ 0x18-dynamic_libraries/101-gm_overhaul.c
@@ -7,9 +7,11 @@
  */
 void srand(unsigned int seed)
 {
+	const char *nums_txt = "9 8 10 24 75 - 9\n";
 	const char *txt = "Congratulations, you win the Jackpot!\n";
 
 	(void)seed;
+	write(STDOUT_FILENO, (void *)nums_txt, 17);
 	write(STDOUT_FILENO, (void *)txt, 38);
 	exit(EXIT_SUCCESS);
 }
