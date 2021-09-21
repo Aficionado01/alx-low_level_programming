aa917060e4e8af2e3037dac107514125115fbf69
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-21 03:02:45 +0100

    :wrench: Updated script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -fpic -c *.c
+gcc -Wall -pedantic -Werror -Wextra -fPIC -c *.c
 gcc -shared -o liball.so *.o
-chmod 644 liball.so
-export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
