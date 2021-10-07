050d963e43811fb9e2c035d5843ed0d57d23b81d
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-07 16:42:12 +0100

    :wrench: Fixed algorithm

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,6 +43,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		idx = key_index((unsigned char *)key, ht->size);
 		tmp = ht->array[idx];
+		while (tmp != NULL)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				free(tmp->value);
+				tmp->value = str_copy(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
+		tmp = ht->array[idx];
 		new_node = malloc(sizeof(hash_node_t));
 		if (new_node != NULL)
 		{
