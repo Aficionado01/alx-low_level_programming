d06b408e3cf3706fc5c33ba28919a54fa37b83e7
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-08 06:43:09 +0100

    :wrench: Added guards for macros

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -3,8 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifndef TRUE
 #define TRUE 1
+#endif
+#ifndef FALSE
 #define FALSE 0
+#endif
 
 /**
  * struct hash_node_s - Node of a hash table
