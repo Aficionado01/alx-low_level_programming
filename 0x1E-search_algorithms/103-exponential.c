6566fdad596c1545c0d5ce7b8bbb0e5bafca353c
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-18 16:36:57 +0100

    Update algorithm

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -68,6 +68,8 @@ int exponential_search(int *array, size_t size, int value)
 
 	if (!array || !size)
 		return (-1);
+	if (array[0] == value)
+		return (0);
 	while (low < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", low, array[low]);
