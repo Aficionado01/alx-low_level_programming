d8c9d4b2e4d11fd7ad7e8183281b5144a7159801
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-18 19:41:22 +0100

    Update algorithm

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,24 @@
 #include "search_algos.h"
 
 /**
+ * print_array - Prints the contents of an array.
+ * @array: The source of the array to print.
+ * @l: The left index of the array.
+ * @r: The right index of the array.
+ */
+void print_array(int *array, int l, int r)
+{
+	size_t i;
+
+	if (l != r)
+	{
+		printf("Searching in array: ");
+		for (i = l; i < l + (r - l + 1); i++)
+			printf("%d%s", *(array + i), i < l + (r - l) ? ", " : "\n");
+	}
+}
+
+/**
  * binary_search_index1 - Searches a value in a sorted array using \
  * a binary search.
  * @array: The array to search in.
@@ -12,42 +30,31 @@
  */
 int binary_search_index1(int *array, size_t l, size_t r, int value)
 {
-	size_t i, m;
-	int index;
+	size_t m;
 
 	if (!array)
 		return (-1);
-	if (l != r)
-	{
-		printf("Searching in array: ");
-		for (i = l; i < l + (r - l + 1); i++)
-			printf("%d%s", *(array + i), i < l + (r - l) ? ", " : "\n");
-	}
 	m = l + ((r - l) / 2);
 	if (l == r)
 		return (*(array + m) == value ? (int)m : -1);
 	if (value < *(array + m))
 	{
-		index = binary_search_index1(array, l, m, value);
-		return (index);
+		print_array(array, l, m);
+		return (binary_search_index1(array, l, m, value));
 	}
 	else if (value == *(array + m))
 	{
 		if ((m > 0) && (*(array + m - 1) == value))
-			index = binary_search_index1(array, l, m, value);
-		else
-			index = (int)m;
-		return (index);
+		{
+			print_array(array, l, m);
+			return (binary_search_index1(array, l, m, value));
+		}
+		return ((int)m);
 	}
 	else
 	{
-		index = binary_search_index1(
-			array,
-			m + 1,
-			r,
-			value
-		);
-		return (index);
+		print_array(array, m + 1, r);
+		return (binary_search_index1(array, m + 1, r, value));
 	}
 }
 
@@ -63,5 +70,8 @@ int advanced_binary(int *array, size_t size, int value)
 {
 	if (!array || !size)
 		return (-1);
+	else if ((size == 1) && (*array == value))
+		return (0);
+	print_array(array, 0, size - 1);
 	return (binary_search_index1(array, 0, size - 1, value));
 }
